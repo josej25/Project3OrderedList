f2371a77648f1097178ad21fc285391fb05b166d
josej25 <113527357+josej25@users.noreply.github.com>
2022-11-14 09:45:04 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,22 +2,23 @@
 #include <string>
 #include "State.h"
 #include <list>
+
 using namespace std;
 
 // add a function to display thr ordered list
-void add(list<int> &l, double x)
+void add(list<State> &l, State &x)
 {
-    list<int>::iterator it;
-    list<int>::iterator previous;
+    list<State>::iterator it;
+    list<State>::iterator previous;
 //    cout<<l.front() << endl;
-    if (l.empty() || (l.front() > x)) {
+    if (l.empty() || (l.front().getPercerntage() > x.getPercerntage())) {
         l.push_front(x);
        // k.push_front(h);
     }
     else{
         previous = l.begin();
         for (auto it = l.begin(); it != l.end(); ++it){
-            if (*it > x)
+            if ((*it).getPercerntage() > x.getPercerntage())
                 break;
             else previous =  it;
         }
@@ -25,22 +26,18 @@ void add(list<int> &l, double x)
         //k.insert(++previous,h);
     }
 }
-void print(list<int> l)
+void print(list<State> l)
 {
     for (auto it = l.begin(); it !=l.end(); it++){
-        cout <<*it << "% ";
+        cout << (*it).getName()<< ": "<<(*it).getPercerntage() << "% "<< endl;
     }
 }
 // add a function to insert the state object into the ordered list.
 int main()
 {
     list <State> myList;
-    list <State>::iterator it;
+    list<State>::iterator it;
     string name;
-    double energy;
-    long long int Size;
-    list<int> ordered;
-    list <State> orderedStateList;
     State tx("Texas",13480.8, 7487580672000);
     tx.findPercentage();
 
@@ -70,28 +67,29 @@ int main()
 
     State mi("Michigan",2610.6, 2696287334400);
     mi.findPercentage();
+    add(myList,tx);
+    add(myList,ca);
+    add(myList,la);
+    add(myList,fl);
+    add(myList,il);
+    add(myList,pa);
+    add(myList,oh);
+    add(myList,ny);
+    add(myList,ga);
+    add(myList,mi);
 
-    myList.push_back(tx);
-    myList.push_back(ca);
-    myList.push_back(la);
-    myList.push_back(fl);
-    myList.push_back(il);
-    myList.push_back(pa);
-    myList.push_back(oh);
-    myList.push_back(ny);
-    myList.push_back(ga);
-    myList.push_back(mi);
+
 
     //add(myList,tx.getPercerntage());
-    for (it = myList.begin(); it !=myList.end(); it++){
+ //   for (it = myList.begin(); it !=myList.end(); it++){
 
-          add(ordered, (*it).getPercerntage());
-          (*it).display();
+   //       add(myList, tx);
+     //     (*it).display();
 
-    }
+    //}
     // add the object to the ordered list
 cout <<endl<< "Percentage of each state's land area:" << endl;
-print(ordered);
+print(myList);
 cout << endl << endl;
 
 
